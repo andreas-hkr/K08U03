f8de00c4bbf1e5b5f31385bc2d27bf4eb4a5d9c2
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 15:08:13 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
+    static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Ange ett tal: ");
